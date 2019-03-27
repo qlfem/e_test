762897379bc6d7e00ae827b93cab0af62a48729d
qlfem <qlfem@nate.com>
2019-03-27 11:47:24 +0900

    code

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 	public static void main(String[] arg) {
 		System.out.println("Hello World");
 		System.out.println("Hello World222");
-		System.out.println("master 333");
+		System.out.println("master");
 	}
 }
